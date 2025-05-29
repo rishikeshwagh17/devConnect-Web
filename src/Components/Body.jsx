b706e275c85f066f2696d8e4982d85a6f8b14caf
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2025-05-29 17:02:24 +0530

    added footer to the application

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Body = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
